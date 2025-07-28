b019bec93a8d03f0e7ea5661f469c5e80ab38b95
hadis <hadisbeigi97@gmail.com>
2025-07-28 09:18:54 +0330

    chore: update dark mode

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -16,12 +16,48 @@ export default function DarkModeToggle() {
   }, [darkMode]);
 
   return (
-    <div className="absolute top-4 right-4 z-50">
+    <div className="flex ">
       <button
-        className="px-3 py-1.5 bg-gray-200 dark:bg-gray-700 text-sm text-gray-900 dark:text-gray-100 rounded-md"
-        onClick={() => setDarkMode(!darkMode)}
+        className=" font-Inter font-normal text-sm   dark:bg-secondary-default   text-black dark:text-white px-1.5 py-1.5 rounded-[32px] flex"
+        onClick={() => setDarkMode(true)}
       >
-        {darkMode ? "☀" : "🌙"}
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 20 20"
+          aria-hidden="true"
+          className="size-4"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        </svg>
+        Escuro
+      </button>
+      <button
+        className="font-Inter font-normal text-sm  bg-secondary-default dark:bg-blackA  text-dark dark:text-white  ml-1 px-1.5 py-1.5 rounded-[32px] flex"
+        onClick={() => setDarkMode(false)}
+      >
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 20 20"
+          aria-hidden="true"
+          className="size-4"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+        Claro
       </button>
     </div>
   );
