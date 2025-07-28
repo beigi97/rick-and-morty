74b5c84beff3f2de66496215c680ef829e6d0f1c
hadis <hadisbeigi97@gmail.com>
2025-07-28 09:04:56 +0330

    refactor(api):replace hardcoded URLs with VITE_API_URL from .env

--- src/api/getEpisodeList.js
+++ src/api/getEpisodeList.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getEpisodeList = async (page = 1) => {
   const response = await axios.get(
-    `https://rickandmortyapi.com/api/episode/?page=${page}`
+    `${import.meta.env.VITE_API_URL}/episode/?page=${page}`
   );
   return response.data;
 };
--- src/api/getFirstPageEpisodes.js
+++ src/api/getFirstPageEpisodes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getFirstPageEpisodes = async () => {
-  const response = await axios.get("https://rickandmortyapi.com/api/episode");
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/episode`);
   return response.data;
 };
